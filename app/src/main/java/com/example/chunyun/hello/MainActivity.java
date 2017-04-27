90f4bf376c7a2729e01630d65d81115875df2032
3June <752473741@qq.com>
2017-04-27 16:08:19 +0800

    I love yamap

--- app/src/main/java/com/example/chunyun/hello/MainActivity.java
+++ app/src/main/java/com/example/chunyun/hello/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -14,4 +14,5 @@ public class MainActivity extends AppCompatActivity {
     //hello春运
     //吹的邮箱有毒
     // mdzz
+    //我爱山p 山p爱我
 }
